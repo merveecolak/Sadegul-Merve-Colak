f380de582f6d8317ae5ada726fa38cc0c13fd774
merveecolak <98888674+merveecolak@users.noreply.github.com>
2022-05-31 14:58:46 +0300

    .

--- BusApp/BusApp.WebUI/Controllers/ExpeditionController.cs
+++ BusApp/BusApp.WebUI/Controllers/ExpeditionController.cs
@@ -22,4 +22,15 @@ namespace BusApp.WebUI.Controllers
             return View(selectedExpedition);
         }
     }
-}       
+}
+
+
+//Dokuz Eylül Üniversitesi  İstatistik (İngilizce) bölümünden onur öğrencisi olarak mezun oldum.
+//Lisans eğitimim boyunca iki farklı şirkette yazılım geliştirme ekibinde aktif olarak rol aldım. Bu süreçte kullandığım teknolojiler :
+//*C# , .NET , R , RStudio , Python
+
+//Üniversiteden mezun olduktan sonra WİSSEN AKADEMİE-BAHÇEŞEHİR ÜNİVERSİTESİ İŞKUR destekli programında Web ve Mobil Yazılım Geliştirme Uzmanı programına 8000 başvuru arasından 5 farklı sınav ve teknik mülakat aşamalarından geçerek ilk 28 öğrencisi arasına girmeye hak kazandım. Bu süreçte kullandığım teknolojiler;
+//*C# , .NET , Entity Framework , Linq , MVC, ASP.NET CORE, ASP.NET WEB API, Generic Repository Design Pattern, Katmanlı Mimari
+//* CSS, HTML, React.js, Javascript, Bootstrap, Jquery
+// * Sql Server, Sqlite
+//*Github, Github Desktop
